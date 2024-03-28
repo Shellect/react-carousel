349d4ca2accd7c30e0aac7acc6d2a2b03917d252
Your name <example@example.com>
2024-03-28 21:26:49 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="carousel">
+                <img src="media/1.jpg" />
+                <img src="media/2.jpg" />
+                <img src="media/3.jpg" />
+                <img src="media/4.jpg" />
+                <img src="media/5.jpg" />
+            </div>
+        );
+    }
+}
