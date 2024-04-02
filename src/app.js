fad795341ac81f4f0f010d17c27a1db3eb6405a9
Your name <example@example.com>
2024-04-02 21:33:12 +0300

    add slide logic

--- src/app.js
+++ src/app.js
@@ -1,14 +1,57 @@
 import React from "react";
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            active: 0
+        }
+        this.images = [
+            "./media/1.jpg",
+            "./media/2.jpg",
+            "./media/3.jpg",
+            "./media/4.jpg",
+            "./media/5.jpg",
+        ];
+        this.len = this.images.length;
+    }
+
+    componentDidMount(){
+        this.timerId = setInterval(
+            () => this.prev(),
+            1000
+        );
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerId);
+    }
+
+    next(){
+        let {active} = this.state;
+        active = ++active % this.len;
+        this.setState({active});
+    }
+
+    prev(){
+        let {active} = this.state;
+        active = (active || this.len) - 1;
+        this.setState({active});
+    }
+
     render() {
+        const {active} = this.state;
+        let images = this.images.map(function (el, i){
+            return <img
+                key={i}
+                className={active === i ? "active" : undefined}
+                src={el} 
+            />
+        });
+
         return (
             <div className="carousel">
-                <img src="media/1.jpg" />
-                <img src="media/2.jpg" />
-                <img src="media/3.jpg" />
-                <img src="media/4.jpg" />
-                <img src="media/5.jpg" />
+                {images}
             </div>
         );
     }
